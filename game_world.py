8ae60d0ce75b369f5e2b65fb5d0eae43acde1e0b
SnoWhite6872 <hanbong34@gmail.com>
2025-11-06 15:54:51 +0900

    4개의 조건 중 하나라도 맞다면

--- game_world.py
+++ game_world.py
@@ -36,4 +36,12 @@ def clear():
 
 
 def collide(a, b):
-    r
+    left_a, bottom_a, right_a, top_a = a.get_bb() #튜플을 받아 4개의 원소를 전달
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
